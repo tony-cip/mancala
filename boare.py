6fc3a487d132964a01cc921607b9e3e171cfa21b
BigT0ny2407 <bigpalmtree30@gmail.com>
2024-11-18 08:33:48 -0500

    putton

---
+++ boare.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+
+game = tk.Tk()
+game.title("Tic Tac Toe")
+
+button_list = []
+theboard = ["" for _ in range(9)]
+# row of buttons
+for row in range (2):
+    # column of buttons
+    for col in range(8):
+        number = row + 3 +col + 1
+        button_number = row * 3 + col   # Calculate button number (1 to 6)
+        button = tk.Button(game,
+                           text = " ",
+                           font = ('Arial', 32),
+                           width = 6,
+                           height = 3,
+                           command = lambda
+                            num=button_number: button_click(num))
+        button.grid( row = row, column = col, padx =2, pady = 2)
+        button_list.append(button)
+
+#run
+game.mainloop()
