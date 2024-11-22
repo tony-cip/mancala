a37fe76b299485b0a13fa447d3bd39a607ecd56e
BigT0ny2407 <bigpalmtree30@gmail.com>
2024-11-22 11:33:39 -0500

    putton

--- boare.py
+++ boare.py
@@ -2,14 +2,26 @@ import tkinter as tk
 
 
 def button_click(num):
-   print(f"Button {num} clicked")  # Placeholder for button functionality
-   temp = num+1
-   for run in range(button_list[num].cget("text")):
-    if temp == 12:
-        temp = 0
-    button_list[temp].config(text=button_list[temp].cget("text")+1)
-    temp+=1
-   button_list[num].config(text = 0)
+    print(f"Button {num} clicked")  # Placeholder for button functionality
+    temp = num+1
+    leftM = True
+    for run in range(button_list[num].cget("text")):
+        if temp == 12:
+            right_mancala.config(text=right_mancala.cget("text")+1)
+            temp = 0
+            continue
+        if temp == 6 and leftM: #if temp is 6 AND left mancala board hasnt been used yet
+            left_mancala.config(text=left_mancala.cget("text")+1)
+            leftM = False
+            continue
+        elif temp == 6: #if temp is 6 and it already went
+            button_list[temp].config(text=button_list[temp].cget("text") + 1)
+            temp+= 1
+            leftM = True
+            continue
+        button_list[temp].config(text=button_list[temp].cget("text")+1)
+        temp+=1
+    button_list[num].config(text = 0)
 
 
 
@@ -23,7 +35,7 @@ theboard = ["" for _ in range(14)]  # 14 slots (12 pits + 2 Mancalas)
 
 # Left Mancala Button
 left_mancala = tk.Button(game,
-                         text= " ",
+                         text=0,
                          font=('Arial', 32),
                          width=6,
                          height=9)
@@ -53,7 +65,7 @@ for col in range(6):
 
 # Right Mancala Button
 right_mancala = tk.Button(game,
-                          text=" ",
+                          text=0,
                           font=('Arial', 32),
                           width=6,
                           height=9)
